eeeaa4079827cba0bb7414faafc39122aa41b61f
Alexander Koskovich <zvnexus@outlook.com>
2021-07-26 01:47:28 -0700

    lahaina: Copy permission for Vulkan dEQP support.
     * Adds files that set the test suite date associated
       with Vulkan dEQP support to either 2019-03-01 or
       2020-03-01.
    
    Change-Id: I5d9993fc7676da475543898b156273a0e37d685f

--- device.mk
+++ device.mk
@@ -23,6 +23,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Adreno
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+
 # Audio
 $(call inherit-product, hardware/qcom-caf/sm8350/audio/configs/lahaina/lahaina.mk)
 $(call inherit-product, vendor/qcom/opensource/audio/configs/qssi/qssi.mk)

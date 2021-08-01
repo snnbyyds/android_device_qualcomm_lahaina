a5725332802535d8f4a6bff5835bcdcb9fa85987
Alexander Koskovich <zvnexus@outlook.com>
2021-07-31 21:10:30 -0700

    lahaina: Add kernel directory to namespaces.
     * In the event of multiple kernel sources the respective
       modules will conflict.
    
    Change-Id: Ibeef1abf654a401c35bec90ab0af9aa79d5bbb5a

--- device.mk
+++ device.mk
@@ -17,7 +17,9 @@ PRODUCT_PACKAGES += \
     otapreopt_script
 
 # Namespaces
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    kernel/qcom/lahaina
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *

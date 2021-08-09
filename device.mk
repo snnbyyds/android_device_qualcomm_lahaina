b91b07196c59c14a4feaba094a4320a0749fcade
Alexander Koskovich <zvnexus@outlook.com>
2021-08-09 03:59:43 -0700

    lahaina: Build remaining OSS dependencies of WFD.
    Change-Id: I7fd3d22b873d0c63f8b7c4a1027c336ae43fb8fd

--- device.mk
+++ device.mk
@@ -198,6 +198,7 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxG711Enc \
     libOmxQcelp13Enc \
+    libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
@@ -227,6 +228,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     kernel/qcom/lahaina
+
+# Networking
+PRODUCT_PACKAGES += \
+    libnl
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *

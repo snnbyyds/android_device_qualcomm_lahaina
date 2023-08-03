b753c5bd15318b3947094813d68a8f1a05e6fe2f
snnbyyds <snnbyyds@gmail.com>
2023-08-03 18:50:14 +0800

    lahaina: Remove unneeded DEVICE_MANIFEST_FILE & DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,14 +79,12 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/hidl/qcom_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml \
     vendor/nxp/nfc/vendor_framework_compatibility_matrix.xml \
     vendor/nxp/secure_element/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
-    $(DEVICE_PATH)/hidl/qcom_manifest.xml \
     $(DEVICE_PATH)/hidl/manifest.xml
 
 DEVICE_MATRIX_FILE := \

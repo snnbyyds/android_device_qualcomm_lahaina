2a7a3ab82c213676dd26450ac2becf6286daf065
snnbyyds <snnbyyds@gmail.com>
2023-08-03 13:03:39 +0800

    Revert "lahaina: Build the fingerprint service and declare support."
    This reverts commit eabf1f38f7f569f906f222c3be60bee234a17781.

--- device.mk
+++ device.mk
@@ -54,13 +54,6 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot
 
-# Biometrics
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
-
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
-
 # Bluetooth
 include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)

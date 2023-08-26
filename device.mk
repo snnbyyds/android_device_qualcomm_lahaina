d466eccb7c6590d8fade7b02a49f74f946bb62bd
snnbyyds <snnbyyds@gmail.com>
2023-08-26 21:30:00 +0800

    lahaina: make sure all kernel modules are installed
    Signed-off-by: snnbyyds <snnbyyds@gmail.com>

--- device.mk
+++ device.mk
@@ -311,7 +311,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Prebuilt Kernel
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)-kernel/dtb.img:dtb.img \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)-kernel/modules/1.1,$(TARGET_COPY_OUT_VENDOR)/lib/modules)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)-kernel/modules/1.1,$(TARGET_COPY_OUT_VENDOR)/lib/modules) \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)-kernel/modules/1.1/5.4-gki,$(TARGET_COPY_OUT_VENDOR)/lib/modules/5.4-gki)
 
 PRODUCT_PACKAGES += \
     lahaina_kernel_recovery
